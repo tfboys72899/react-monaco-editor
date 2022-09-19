cdf3b8ce5856eca88e533eb6ca2b856a18dc9528
tfboys72899 <728991524@qq.com>
2022-09-19 09:31:25 +0800

    modified:   example/index.js     delete the test code

--- example/index.js
+++ example/index.js
@@ -12,11 +12,6 @@ import 'antd/dist/antd.css';
 moment.locale('zh-cn');
 
 const App = () => {
-  const [date, setDate] = useState(null);
-  const handleChange = value => {
-    message.info(`您选择的日期是: ${value ? value.format('YYYY年MM月DD日') : '未选择'}`);
-    setDate(value);
-  };
   return (
     <div id='content'>
       <DocList></DocList>
