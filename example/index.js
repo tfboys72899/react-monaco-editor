1c8591310190caa53cad591d84f2c7688cc89954
tfboys72899 <728991524@qq.com>
2022-09-19 11:01:36 +0800

    modified:   example/index.css 	modified:   example/index.js

--- example/index.js
+++ example/index.js
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
 import DocList from './docList';
-
-import { message } from 'antd';
 // 由于 antd 组件的默认文案是英文，所以需要修改为中文
 import moment from 'moment';
 import 'moment/locale/zh-cn';
